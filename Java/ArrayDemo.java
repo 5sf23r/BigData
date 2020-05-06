5771b84edc07ab8a90ffe85340956e4f3b683674
5sf23r <5sf23r@gmail.com>
2020-05-07 00:35:39 +0800

    2020.5.7

---
+++ Java/ArrayDemo.java
@@ -0,0 +1,8 @@
+class ArrayDemo{
+    public static void main(String[] args){
+        int[] arr=new int[2];
+        int[] arr1;
+        arr1=new int[3];
+        System.out.println(arr);
+    }
+}
