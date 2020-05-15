3c7154ac2cd1626bd93738b3c809b809ef1c8ac2
5sf23r <5sf23r@gmail.com>
2020-05-15 15:48:40 +0800

    2020.5.15

---
+++ Java/PackageDemo.java
@@ -0,0 +1,6 @@
+package cm.tedu.exer;
+class PackageDemo{
+    public static void main(String[] args){
+        System.out.println("OK");
+    }
+}
---
+++ Java/PackageDemo1.java
@@ -0,0 +1,6 @@
+package cn.tedu.exer;
+class PackageDemo1{
+    public static void main(String[] args){
+        System.out.println("OK");
+    }
+}
