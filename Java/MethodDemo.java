fbdcdc6275a312938c450840e5dd9229161f597a
5sf23r <5sf23r@gmail.com>
2020-05-15 18:01:55 +0800

    2020.5.15

---
+++ Java/MethodDemo.java
@@ -0,0 +1,14 @@
+package cn.commandoptionsec.method;
+public class MethodDemo{
+    public static void main(String[] args){
+        printXing(3,6);
+    }
+    public static void paintXing(int x,int y){
+        for(int i=1;i<=8;i++){
+                    for(int j=1;j<=6;j++){
+                        System.out.print("*");
+                    }
+                    System.out.println();
+                }
+    }
+}
