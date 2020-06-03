cdd8632da7bd88b0f6df6eb685cca10658aecea4
5sf23r <5sf23r@gmail.com>
2020-06-03 17:40:39 +0800

    2020.6.3

--- Java/Ex.java
+++ Java/Ex.java
@@ -6,16 +6,19 @@ public class Ex{
 
         changeArray(a);
         System.out.println(a[0]);
+
         printArray(a);
     }
     public static void expand(int[] a){
         int[] newArray=new int[a.length*2];
         System.arraycopy(a,0,newArray,0,a.length);
         a=newArray;
+
         for(int i=0;i<a.length;i++){
             System.out.print(a[i]+"\t");
         }
         System.out.println();
+
         System.out.println(a);
     }
     public static void changeArray(int[] a){
