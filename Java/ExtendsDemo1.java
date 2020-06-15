d5c7fb90b4293bf0f434edd9ca9cf6d8d201af47
5sf23r <5sf23r@gmail.com>
2020-06-15 18:05:13 +0800

    2020.6.15

---
+++ Java/ExtendsDemo1.java
@@ -0,0 +1,17 @@
+package cn.commandoptionesc.extendsx;
+public class ExtendsDemo1{
+    public static void main(String[] args){
+        
+    }
+}
+
+//
+class A{
+    public void m(){
+        
+    }
+}
+
+class B extends A{
+    public void m(){}
+}
