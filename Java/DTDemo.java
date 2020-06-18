f00d1432f9684a5441c76ac213419eada4e63fb6
5sf23r <5sf23r@gmail.com>
2020-06-18 17:49:37 +0800

    2020.6.18

--- Java/DTDemo.java
+++ Java/DTDemo.java
@@ -8,7 +8,18 @@ public class DTDemo{
         //调用方法
         //向上造型创建的对象调用方法执行，由子类具体执行
         p.eat();
-        p.play();
+
+        //向上造型创建的对象可以调用哪些方法看父类
+        // p.play();
+        p.sleep();
+        n(p);
+        n(new Dog());
+        n(new Cat());
+    }
+    //参数的统一
+    //只要等到运行时期才能明确向上造型的对象由谁实际创建
+    public static void n(Pet p){
+        p.eat();
     }
 }
 //宠物类
@@ -17,10 +28,13 @@ class Pet{
     public void eat(){
         System.out.println("在吃东西...");
     }
+    /* public void play(){
+        System.out.println();
+        }*/
     public void sleep(){
         System.out.println("在睡觉...");
     }
-}
+} 
 
 //猫
 class Cat extends Pet{
