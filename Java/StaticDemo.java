ca75a7cdb9c3d9fb1d42db54be86634acae06454
5sf23r <5sf23r@gmail.com>
2020-06-22 18:05:30 +0800

    2020.6.22

---
+++ Java/StaticDemo.java
@@ -0,0 +1,39 @@
+package cn.commandoptionesc.staticx;
+public class StaticDemo{
+    public static void main(String[] args){
+        //创建对象
+        Person p=new Person();
+        //赋值
+        p.name="梅超风";
+        p.age=30;
+        p.gender='女';
+         
+        Person p1=new Person();
+        p1.name="欧阳锋";
+        p1.age=50;
+        p1.gender='男';
+
+        Person p2=new Person();
+        p2.name="lili";
+        p2.age=43;
+        p2.gender='中';
+        
+        System.out.println(p2.toString());
+        System.out.println(p1.toString());
+        System.out.println(p.toString());
+    }
+}
+//Personl类
+class Person{
+    //属性
+    String name;
+    int age;
+    //static修饰变量
+    static char gender;
+
+
+    //toString()方法
+    public String toString(){
+        return "name:"+name+",age:"+age+",gender:"+gender;
+    }
+}
