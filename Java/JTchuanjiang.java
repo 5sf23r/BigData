6b78a6287e20bc41b5fc5fd2a94325381a96a452
5sf23r <5sf23r@gmail.com>
2020-06-23 00:02:13 +0800

    2020.6.23

---
+++ Java/JTchuanjiang.java
@@ -0,0 +1,27 @@
+package cn.commandoptionesc.staticx;
+public class JTchuanjiang{
+    public sattic void main(String[] args){
+
+    }
+}
+class {
+
+
+    int age;
+    static String adress;
+    public void print(){
+        System.out.println(name+":"+age+":"+address);
+    }
+    public static void say(){
+        System.out.println("heheh");
+    }
+    public static void say(String name){
+        System.out.println("hehehehe1");
+        return 1;
+    }
+}
+class Person extends Person1{
+    public static void say(){
+
+    }
+}
