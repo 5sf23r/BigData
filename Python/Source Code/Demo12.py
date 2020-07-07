0682e75fd7b46534e3d4377dcf54657ab6189595
5sf23r <5sf23r@gmail.com>
2020-07-07 18:01:51 +0800

    2020.7.7

--- Python/Source Code/Demo12.py
+++ Python/Source Code/Demo12.py
@@ -1,7 +1,8 @@
 msg01 = input("请输入内容:")
 msg02 = input("请继续输入内容：")
-temp = msg02
-msg02 = msg01
-msg01 = temp
+#temp = msg02
+#msg02 = msg01
+#msg01 = temp
+msg01,msg02 = msg02,msg01
 print("msg01 = " +  msg01)
 print("msg02 = " +  msg02)
---
+++ Python/Source Code/Demo13.py
@@ -0,0 +1,8 @@
+str_price = input("请输入商品的单价：")
+str_num = input("请输入购买的数量")
+str_money = input("请输入支付的金额")
+float_price = float(str_price)
+int_num = int(str_num)
+money02 = int(money01)
+yu = money02 - price02 * num02
+print("应该找回钱数"+str (yu))
