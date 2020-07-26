beb5d31269e9307a52431e60c8dcd26bd70d5483
5sf23r <5sf23r@gmail.com>
2020-07-27 02:32:55 +0800

    2020.7.27

---
+++ Another/Java/Source Code/Object03/src/cn/commandoptionesc/extendsx/ExtendsDemo.java
@@ -0,0 +1,8 @@
+package cn.commandoptionesc.extendsx;
+
+public class ExtendsDemo {
+}
+//定义一个类表示猫
+class Cat{
+
+}
--- Another/Java/Source Code/Object03/src/cn/commandoptionesc/object/PrivateDemo.java
+++ Another/Java/Source Code/Object03/src/cn/commandoptionesc/object/PrivateDemo.java
@@ -3,14 +3,36 @@ package cn.commandoptionesc.object;
 public class PrivateDemo {
     public static void main(String[] args) {
         Person p = new Person();
-        p.name = "amy";
-        p.age = 16;
+        p.setName("amy");
+        p.setAge(17);
+        System.out.println(p.getAge());
+        System.out.println(p.getName());
     }
 }
 
 //定义一个代表人的类
 class Person{
 
-    String name;
+    private String name;
+    //私有
     private int age;
+
+    //提供一个方法，将要赋值的属性作为参数
+    public void setAge(int age){
+        if(age < 0 || age >= 150)
+            return;
+        this.age = age;
+    }
+
+    public int getAge(){
+        return this.age;
+    }
+
+    public void setName(String name){
+        this.name = name;
+    }
+
+    public String getName(){
+        return this.name;
+    }
 }
