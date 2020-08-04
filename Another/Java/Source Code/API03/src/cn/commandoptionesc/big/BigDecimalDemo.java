34f892d82d32129dff6df07f5c6e6489614fe458
5sf23r <5sf23r@gmail.com>
2020-08-05 02:10:01 +0800

    2020.8.5

--- Another/Java/Source Code/API03/src/cn/commandoptionesc/big/BigDecimalDemo.java
+++ Another/Java/Source Code/API03/src/cn/commandoptionesc/big/BigDecimalDemo.java
@@ -1,7 +1,29 @@
 package cn.commandoptionesc.big;
 
+import java.math.BigDecimal;
+
 public class BigDecimalDemo {
     public static void main(String[] args) {
 
+        //小数在计算的时候无法保证精确
+        double d1 = 3.31;
+        double d2 = 2.97;
+        System.out.println(d1 - d2);
+
+        //如果需要对小数运算保证精确性，那么需要BigDecimal
+        BigDecimal b1 = new BigDecimal(3.31);
+        BigDecimal b2 = new BigDecimal(2.97);
+        //等价于3.31 - 2.97
+        System.out.println(b1.subtract(b2));
+        //等价于3.31 + 2.97
+        System.out.println(b1.add(b2));
+
+        //如果需要BigDecimal进行精确运算，那么需要将参数以字符串形式来传入
+        //当字符串传形式传入的时候，在底层会以字符数组形式来计算
+        BigDecimal b3 = new BigDecimal("3.31");
+        BigDecimal b4 = new BigDecimal("2.97");
+
+        System.out.println(b3.subtract(b4));
+        System.out.println(b3.add(b4));
     }
 }
---
+++ Another/Java/Source Code/API03/src/cn/commandoptionesc/big/BigIntegerDemo.java
@@ -0,0 +1,11 @@
+package cn.commandoptionesc.big;
+
+import java.math.BigInteger;
+
+public class BigIntegerDemo {
+    public static void main(String[] args) {
+
+        BigInteger b1 = new BigInteger("234234256325345325325");
+        BigInteger b2 = new BigInteger("3435352345234642341234252345");
+    }
+}
